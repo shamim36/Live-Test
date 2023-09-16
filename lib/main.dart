687db04fdc624d8fd2706864181f7690a2dcc82e
Md Shamim Ahamed <md.shamim.ahamed.888@gmail.com>
2023-09-16 23:21:27 +0600

    module06-done

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp();
+void main() {
+  runApp(const MyApp());
+}
 
-class runApp extends StatelessWidget {
-  const runApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Assignment Module 06',
-      home: Home(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Module 06 Assignemt Ostad Flutter',
+      home: MyHomePage(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('My Shopping List')),
+        backgroundColor: Colors.blueAccent,
+        titleTextStyle: const TextStyle(
+            color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+        actions: const [
+          Icon(
+            Icons.shopping_cart,
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: const [
+          ListTile(
+            leading: Icon(Icons.shopping_basket_rounded),
+            title: Text('Apples'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket_rounded),
+            title: Text('Bananas'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket_rounded),
+            title: Text('Bread'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket_rounded),
+            title: Text('Milk'),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_basket_rounded),
+            title: Text('Eggs'),
+          ),
+        ],
+      ),
+    );
   }
 }
